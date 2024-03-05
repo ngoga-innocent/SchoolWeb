50502c03dc746a45e2c0c8f229ef5c2667ed618c
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-03-05 18:39:12 +0200

    statics

--- SchoolWeb/settings.py
+++ SchoolWeb/settings.py
@@ -146,6 +146,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Path to the root static directory
 ]
 
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
