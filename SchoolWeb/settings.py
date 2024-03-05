f9b70fd8815223368ba9dd38047cd31aab9cb3da
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-03-05 14:03:06 +0200

    check allowed

--- SchoolWeb/settings.py
+++ SchoolWeb/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower()=="True"
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS=os.environ.get("ALLOWED_HOST")
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS=os.environ.get("ALLOWED_HOST")
 
 
 # Application definition
