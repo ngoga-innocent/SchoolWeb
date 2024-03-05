44df26968b680b1da9fae7cd4a0ac54480608662
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-03-05 19:39:59 +0200

    images

--- SchoolWeb/settings.py
+++ SchoolWeb/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Home/static'),  # Path to the root static directory
