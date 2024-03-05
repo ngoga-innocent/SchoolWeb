ac3fadffcc1df6f583843fb6b49fc871f3a6151f
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-03-05 14:39:49 +0200

    try again

--- SchoolWeb/settings.py
+++ SchoolWeb/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@rzt7wh56acny3grd15mq5(3p@k*-4fr7ws5=y1mm=vo7bw(tz'
 #SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG","False").lower()=="True"
-ALLOWED_HOSTS=[]
+DEBUG ="False"
+ALLOWED_HOSTS=['*']
 #ALLOWED_HOSTS = ['schoolweb-4kif.onrender.com']
 #ALLOWED_HOSTS=os.environ.get("ALLOWED_HOST")
 
